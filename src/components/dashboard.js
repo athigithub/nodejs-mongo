40a0bbe2c5ab4572bbc28d483944ddbcd001dab6
athigithub <athiz.inmakes@gmail.com>
2023-07-07 21:20:15 +0530

    saved

---
+++ src/components/dashboard.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <header className="dashboard-header">
+        <h1>Dashboard</h1>
+      </header>
+      <div className="dashboard-content">
+        <div className="sidebar">
+          {/* Sidebar content goes here */}
+        </div>
+        <div className="main-content">
+          {/* Main content goes here */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
